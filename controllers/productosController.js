dbfcde7a6818162c848cc982551635881f7aeded
Jorge Hernández <jorge.hernandez1326@alumnos.udg.mx>
2021-11-10 13:12:52 -0600

    modificaciones

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -3,14 +3,7 @@ const controlador = {
         res.render('productos/detalleProducto');
     },
     carritoCompra: function(req,res) {
-<<<<<<< HEAD
         res.render('productos/carritoCompra');
-=======
-        res.render('carritoCompra');
-    },
-    agregarProducto: function(req,res) {
-        res.render('agregarProducto');
->>>>>>> 94bc4490da1767640472854012433b266a7e2b66
     }
 }
 
