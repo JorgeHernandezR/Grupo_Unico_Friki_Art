7f9e85b11cf84c7868d9eda57cf217aa5cd47d51
SalvadorMendozaB <SalvadorMen3@gmail.com>
2021-11-10 13:13:24 -0600

    Correcciones menores

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -3,20 +3,13 @@ const controlador = {
         res.render('productos/detalleProducto');
     },
     carritoCompra: function(req,res) {
-<<<<<<< HEAD
         res.render('productos/carritoCompra');
-=======
-        res.render('carritoCompra');
     },
     agregarProducto: function(req,res) {
-        res.render('agregarProducto');
-<<<<<<< HEAD
+        res.render('productos/agregarProducto');
     },
     editarProducto: function(req,res) {
-        res.render('editarProducto');
-=======
->>>>>>> 94bc4490da1767640472854012433b266a7e2b66
->>>>>>> c83c999e0d155ee252e5d94ef539b72982aee7e0
+        res.render('productos/editarProducto');
     }
 }
 
