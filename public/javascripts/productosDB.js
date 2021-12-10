e2e71e8eb72033ae1f9177c9855cab22c4b042b5
SalvadorMendozaB <SalvadorMen3@gmail.com>
2021-12-09 19:10:11 -0600

    Añadido de la plantilla del mdulo de productosDB

---
+++ public/javascripts/productosDB.js
@@ -0,0 +1,19 @@
+const productosDB = {
+    obtenerTodos: function () {
+
+    },
+    agregarProducto: function () {
+
+    },
+    obtenerProducto: function () {
+
+    },
+    actaulizarProducto: function () {
+
+    },
+    eliminarProducto: function () {
+
+    }
+}
+
+module.exports = productosDB;
